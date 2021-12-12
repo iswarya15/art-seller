4cddadf688738b5055c823147a28170285c083fb
iswarya <iswarya.ch99@gmail.com>
2021-12-12 17:32:27 +0530

    created menu and directory component from homepage

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import './directory.styles.scss'
+import  Women from '../../assets/women.jpg'
+import  Men  from '../../assets/men.jpg'
+class Directory extends React.Component{
+
+  constructor(){
+   super();
+    this.state = {
+        sections:[
+        {
+            title: 'hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            id: 1,
+            linkUrl: 'shop/hats'
+        },
+        {
+            title: 'jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            id: 2,
+            linkUrl: 'shop/jackets'
+        },
+        {
+            title: 'sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id: 3,
+            linkUrl: 'shop/sneakers'
+        },    
+        {    
+            title: 'womens',
+            imageUrl: Women,
+            size: 'large',
+            id: 4,
+            linkUrl: 'shop/womens'
+        },    
+        {    
+            title: 'mens',
+            imageUrl: Men,
+            size: 'large',
+            id: 5,
+            linkUrl: 'shop/mens'
+        }   
+        ]
+    }
+}
+ render(){
+    return (
+      <div className="directory-menu">
+         { this.state.sections.map(({ id ,title, imageUrl, size}) => (
+               <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+            ))
+         }
+      </div>
+    )
+}
+}
+export default Directory;
