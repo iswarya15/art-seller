382e2823b96acc40cf0c5a5a191e09c2ed8fad9d
iswarya <iswarya.ch99@gmail.com>
2021-12-12 15:01:24 +0530

    added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import './homepage.styles.scss';
+export const HomePage = () => (
+
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Music
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Painting
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Biography
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Poetry
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        Novel
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+)
